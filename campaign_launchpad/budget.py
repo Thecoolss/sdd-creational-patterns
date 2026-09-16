ab450aa2a83ecb127a0f40472fb7f6d65121ab45
Sami Droubi <sami.t.droubi@gmail.com>
2026-09-16 15:41:34 +0200

    Add singleton budget

--- campaign_launchpad/budget.py
+++ campaign_launchpad/budget.py
@@ -6,12 +6,15 @@ class GlobalBudget:
     _instance = None
 
     def __new__(cls, initial_amount: float = 0.0):
-      # TODO: Singleton pattern implementation
-      pass
+      if cls._instance is None:
+        cls._instance = super().__new__(cls)
+        cls._instance._balance = initial_amount
+      return cls._instance
 
     def allocate(self, amount: float) -> None:
-      # TODO: Allocate amount from the budget
-      pass
+      if amount <= 0 or self._balance < amount:
+        raise ValueError("Insufficient budget")
+      self._balance -= amount
 
     def remaining(self) -> float:
         return self._balance
